d4c1ed31ea30c6c92f7a6508a7495400abd98b83
Harsh Chokshi <harshchokshi@gmail.com>
2015-09-19 17:43:57 +1200

    Create worker for TTS previews

---
+++ src/com/softeng206/vidivox/concurrency/FestivalPreviewWorker.java
@@ -0,0 +1,36 @@
+package com.softeng206.vidivox.concurrency;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * @author Harsh Chokshi
+ */
+public class FestivalPreviewWorker extends BashWorker {
+    private String message;
+
+    public FestivalPreviewWorker(String message) {
+        this.message = message;
+    }
+
+    protected String getBashCommand() {
+        // TODO: escape message for characters like "
+        return "echo \"" + message + "\" | festival --tts";
+    }
+
+    protected int getKillPID(int mainPid) {
+        try {
+            ProcessBuilder cancelBuilder = new ProcessBuilder("/bin/bash", "-c", "pstree -p " + mainPid);
+            Process process = cancelBuilder.start();
+            BufferedReader stdout = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = stdout.readLine();
+
+            // Extract number between play( ) giving PID of aplay / play
+            line = line.split("play\\(")[1].split("\\)")[0];
+
+            return Integer.parseInt(line);
+        } catch (Exception f) {}
+
+        throw new RuntimeException("Could not evaluate ID of festival player process.");
+    }
+}
