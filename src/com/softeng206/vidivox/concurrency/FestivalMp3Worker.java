9dc550c2efbd7517677e7fb59a0d58fd18df6cfa
Harsh Chokshi <harshchokshi@gmail.com>
2015-09-21 20:26:12 +1200

    Overwrite files when accepted in file chooser

--- src/com/softeng206/vidivox/concurrency/FestivalMp3Worker.java
+++ src/com/softeng206/vidivox/concurrency/FestivalMp3Worker.java
@@ -15,6 +15,11 @@ public class FestivalMp3Worker extends BashWorker {
     }
 
     protected String getBashCommand() {
+        // If we get here, the overwrite option in the file chooser was accepted
+        if (destination.exists()) {
+            destination.delete();
+        }
+
         // FFMPEG command from Nasser's followup to https://piazza.com/class/icl94md3xuv6n9?cid=92
         // Makes temporary wav with text2wave, then converts to MP3 using ffmpeg
         return "rm -f /tmp/vidivoxout.wav && echo \"" + escapeChars(message) + "\" | text2wave -o /tmp/vidivoxout.wav && " +
