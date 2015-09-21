8258989921dfd35e71035331d688e278633d5630
Harsh Chokshi <harshchokshi@gmail.com>
2015-09-21 18:52:02 +1200

    Fix FestivalMp3Worker PID

--- src/com/softeng206/vidivox/concurrency/FestivalMp3Worker.java
+++ src/com/softeng206/vidivox/concurrency/FestivalMp3Worker.java
@@ -24,6 +24,6 @@ public class FestivalMp3Worker extends BashWorker {
 
     protected int getKillPID(int mainPid) {
         // TODO: analyse text2wave / ffmpeg process tree, if we want to have cancel functionality
-        return 0;
+        return mainPid;
     }
 }
